f6d1b49cf4c280f8711ab8362e94079581ea08ac
Tarcisio Araujo <tarcisiovie3134@gmail.com>
2026-02-17 00:07:10 -0300

    Recriar app.js funcionando

--- js/app.js
+++ js/app.js
@@ -1,15 +1,25 @@
-fetch('/filmes.json')
+﻿fetch('/filmes.json')
 .then(r => r.json())
 .then(lista => {
     const c = document.getElementById('container');
     c.innerHTML = '';
+
     lista.forEach(f => {
-        c.innerHTML += 
-        <div class="filme">
+        const div = document.createElement('div');
+        div.className = 'filme';
+
+        div.innerHTML = 
             <img src="\">
             <h3>\</h3>
             <p>\</p>
             <a href="\" target="_blank">Trailer</a>
-        </div>;
+        ;
+
+        c.appendChild(div);
     });
+})
+.catch(err => {
+    document.getElementById('container').innerHTML =
+        "<p>Erro ao carregar filmes.</p>";
+    console.log(err);
 });
