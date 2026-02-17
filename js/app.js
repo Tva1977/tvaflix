1fc4f734ab478a4783b2df8b9982b47f9943eba2
Tarcisio Araujo <tarcisiovie3134@gmail.com>
2026-02-17 00:02:28 -0300

    Estrutura profissional do projeto

---
+++ js/app.js
@@ -0,0 +1,15 @@
+fetch('filmes.json')
+.then(r => r.json())
+.then(lista => {
+    const c = document.getElementById('container');
+    c.innerHTML = '';
+    lista.forEach(f => {
+        c.innerHTML += 
+        <div class="filme">
+            <img src="\">
+            <h3>\</h3>
+            <p>\</p>
+            <a href="\" target="_blank">Trailer</a>
+        </div>;
+    });
+});
