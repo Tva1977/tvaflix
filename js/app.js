dfcad5fa3d858d59d98746f7dd7467cfb0c31df9
Tarcisio Araujo <tarcisiovie3134@gmail.com>
2026-02-17 00:10:44 -0300

    Estrutura completa do TVAFLIX

--- js/app.js
+++ js/app.js
@@ -1,25 +1,15 @@
-﻿fetch('/filmes.json')
+fetch('filmes.json')
 .then(r => r.json())
 .then(lista => {
     const c = document.getElementById('container');
     c.innerHTML = '';
-
     lista.forEach(f => {
-        const div = document.createElement('div');
-        div.className = 'filme';
-
-        div.innerHTML = 
-            <img src="\">
-            <h3>\</h3>
-            <p>\</p>
-            <a href="\" target="_blank">Trailer</a>
-        ;
-
-        c.appendChild(div);
+        c.innerHTML += 
+        <div class="filme">
+            <img src="">
+            <h3></h3>
+            <p></p>
+            <a href="" target="_blank">Trailer</a>
+        </div>;
     });
-})
-.catch(err => {
-    document.getElementById('container').innerHTML =
-        "<p>Erro ao carregar filmes.</p>";
-    console.log(err);
 });
