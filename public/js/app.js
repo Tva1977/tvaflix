3845a20048ac13520ab0f3135aad43cfb3abf886
Tarcisio Araujo <tarcisiovie3134@gmail.com>
2026-03-01 13:12:06 -0300

    TVAFLIX 2026 - Layout Netflix final com filmes, séries e trailers adicionados

--- public/js/app.js
+++ public/js/app.js
@@ -3,9 +3,6 @@
 async function carregarTudo(){
     const filmes = await fetchJSON('filmes.json');
     const series = await fetchJSON('series/series.json');
-    const desenhos = await fetchJSON('desenhos/desenhos.json');
-    const infantil = await fetchJSON('infantil/infantil.json');
-    const documentarios = await fetchJSON('documentarios/documentarios.json');
 
     const render = (arr, id, prefix='')=>{
         const container = document.getElementById(id);
@@ -14,17 +11,13 @@ async function carregarTudo(){
             <div class='item'>
                 <img src='\' alt='\'>
                 <p>\</p>
-                <button onclick="window.open('player/player.html?video=\\.mp4')">Assistir</button>
-                <button onclick="window.open('player/player.html?video=\\')">Trailer</button>
+                <button onclick=\"window.open('\')\">Assistir</button>
+                <button onclick=\"window.open('\\")\">Trailer</button>
             </div>;
         });
     }
 
     render(filmes,'filmes');
-    render(series,'series','series/');
-    render(desenhos,'desenhos','desenhos/');
-    render(infantil,'infantil','infantil/');
-    render(documentarios,'documentarios','documentarios/');
+    render(series,'series');
 }
-
 carregarTudo();
